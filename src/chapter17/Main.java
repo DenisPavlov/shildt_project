eb7ab215429947cefec446365168eb7fed81a4b9
Денис Павлов <denis.pavlov@digital-ecosystems.ru>
2018-06-29 10:44:09 +0700

    начал читать главу 17

---
+++ src/chapter17/Main.java
@@ -0,0 +1,11 @@
+package chapter17;
+
+public class Main {
+    public static void main(String[] args) {
+        Double doub1 = new Double("3.5");
+        Double doub2 = new Double("3.5");
+        System.out.println(doub1);
+        System.out.println(doub2);
+        System.out.println(doub1/doub1);
+    }
+}
