aedcff85223cc311e3c6cfef188fac5d29c5958c
Денис Павлов <denis.pavlov@digital-ecosystems.ru>
2018-06-29 17:19:20 +0700

    начал читать главу 17

--- src/chapter17/Main.java
+++ src/chapter17/Main.java
@@ -2,10 +2,15 @@ package chapter17;
 
 public class Main {
     public static void main(String[] args) {
-        Double doub1 = new Double("3.5");
-        Double doub2 = new Double("3.5");
-        System.out.println(doub1);
-        System.out.println(doub2);
-        System.out.println(doub1/doub1);
+//        Double doub1 = new Double("3.5");
+//        Double doub2 = new Double("3.5");
+//        System.out.println(doub1);
+//        System.out.println(doub2);
+//        System.out.println(doub1/doub1);
+//
+        String s = "Hello";
+        String a = "Hello";
+        System.out.println(a == s);
+        System.out.println(300 == 300L);
     }
 }
